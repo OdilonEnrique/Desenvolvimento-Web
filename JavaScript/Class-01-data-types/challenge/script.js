fe8b69a95b86eee83514ba7b23ccb0fa0e017110
Odilon Enrique <odilonegduarte@gmail.com>
2023-09-15 20:43:09 -0300

    update js

--- JavaScript/Class-01-data-types/challenge/script.js
+++ JavaScript/Class-01-data-types/challenge/script.js
@@ -6,6 +6,6 @@ let number1 = Number(prompt("Digite o primeiro número"));
 
 let number2 = Number(prompt("Digite o segundo número"));
 
-let soma = Number(number1 + number2);
+const soma = Number(number1 + number2);
 
 alert("A soma de " + number1 + "+" + number2 + "=" + soma);
