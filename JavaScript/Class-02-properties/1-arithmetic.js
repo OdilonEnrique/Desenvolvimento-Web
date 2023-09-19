1e2b9d92f231ffb902074da251ca0d8af982b983
Odilon Enrique <odilonegduarte@gmail.com>
2023-09-19 18:48:35 -0300

    add challenge

---
+++ JavaScript/Class-02-properties/1-arithmetic.js
@@ -0,0 +1,26 @@
+let num1 = 2;
+let num2 = 3;
+let numString = "3";
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 - numString);
+console.log(num1 + numString);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2); // Potenciação
+console.log(81 ** (1 / 2)); // Raíz, pra mudar o exponencial é só mudar o valor do denominador
+console.log(16 ** (1 / 4));
+console.log(Math.sqrt(49)); // Outra forma de usar raiz
+console.log(++num1);
+console.log(--num1);
+
+// num1 += 5;
+// num1 -= 5;
+// num1 *= 5;
+// num1 /= 5;
+num1 %= 5;
+// num1 **= 5;
+
+console.log(num1);
---
+++ JavaScript/Class-02-properties/2-comparation.js
@@ -0,0 +1,16 @@
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(10 === 10); // É igual no valor e no tipo(Se os dois são Strings ou números)
+console.log(10 == 10);
+console.log(10 >= 10);
+console.log(10 <= 10);
+console.log(10 != 10);
+console.log(10 !== 10);
+console.log(10 !== 20); // É desigual o valor e o tipo das variaveis
+console.log(10 != 20);
+console.log(10 <= 20);
+console.log(10 >= 20);
+console.log(10 > 20);
+console.log(10 < 20);
+console.log(10 < 20);
+console.log(10 === "10");
