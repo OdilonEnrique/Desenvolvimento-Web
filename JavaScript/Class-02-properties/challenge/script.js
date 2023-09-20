18880dc37f39d016e89328f79c1109a44fdaf864
Odilon Enrique <odilonegduarte@gmail.com>
2023-09-20 14:10:30 -0300

    update script challenge

--- JavaScript/Class-02-properties/challenge/script.js
+++ JavaScript/Class-02-properties/challenge/script.js
@@ -10,10 +10,10 @@ let rest = num1 % num2;
 let pot = num1 ** num2;
 let raiz1 = num1 ** (1/num2);
 
-document.write("A soma de " + num1 + " e " + num2 + " = " + soma + "<br>");
-document.write("A subtração de " + num1 + " e " + num2 + " = " + sub + "<br>");
-document.write("A multiplicação de " + num1 + " e " + num2 + " = " + multi + "<br>");
-document.write("A divisão de " + num1 + " e " + num2 + " = " + div + "<br>");
-document.write("O resto da divisão de " + num1 + " e " + num2 + " = " + rest + "<br>");
-document.write("A potência de " + num1 + " elevado a " + num2 + " = " + pot + "<br>");
-document.write("A raiz " + num1 + " de  " + num2 + " = " + raiz1 + "<br>");
+document.write("A soma de " + num1 + " e " + num2 + " = " + soma.toFixed(2) + "<br>");
+document.write("A subtração de " + num1 + " e " + num2 + " = " + sub.toFixed(2) + "<br>");
+document.write("A multiplicação de " + num1 + " e " + num2 + " = " + multi.toFixed(2) + "<br>");
+document.write("A divisão de " + num1 + " e " + num2 + " = " + div.toFixed(2) + "<br>");
+document.write("O resto da divisão de " + num1 + " e " + num2 + " = " + rest.toFixed(2) + "<br>");
+document.write("A potência de " + num1 + " elevado a " + num2 + " = " + pot.toFixed(2) + "<br>");
+document.write("A raiz " + num1 + " de  " + num2 + " = " + raiz1.toFixed(2) + "<br>");
