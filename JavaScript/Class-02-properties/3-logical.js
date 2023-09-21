5d5a98c9a2b1d7eefad15943f26a25b96c671aab
Odilon Enrique <odilonegduarte@gmail.com>
2023-09-21 15:28:45 -0300

    add files

---
+++ JavaScript/Class-02-properties/3-logical.js
@@ -0,0 +1,17 @@
+let creditCard = true;
+let credit = true;
+let hasCredit = false;
+
+console.log(creditCard);
+console.log(credit);
+console.log(!credit);
+console.log(creditCard && credit);
+console.log(creditCard && hasCredit);
+
+const saveMoney = true;
+const getLoan = true;
+
+console.log(!saveMoney || getLoan);
+console.log(saveMoney || getLoan);
+console.log(saveMoney || !getLoan);
+console.log(saveMoney || !getLoan);
--- JavaScript/Class-02-properties/challenge/script.js
+++ JavaScript/Class-02-properties/challenge/script.js
@@ -2,18 +2,18 @@ let num1 = Number(prompt("Digite o primeiro número"));
 
 let num2 = Number(prompt("Digite o segundo número"));
 
-let soma = num1 + num2;
-let sub = num1 - num2;
-let multi = num1 * num2;
-let div = num1 / num2;
-let rest = num1 % num2;
-let pot = num1 ** num2;
-let raiz1 = num1 ** (1/num2);
+const soma = num1 + num2;
+const sub = num1 - num2;
+const multi = num1 * num2;
+const div = num1 / num2;
+const rest = num1 % num2;
+const pot = num1 ** num2;
+const raiz1 = num1 ** (1/num2);
 
-document.write("A soma de " + num1 + " e " + num2 + " = " + soma.toFixed(2) + "<br>");
-document.write("A subtração de " + num1 + " e " + num2 + " = " + sub.toFixed(2) + "<br>");
-document.write("A multiplicação de " + num1 + " e " + num2 + " = " + multi.toFixed(2) + "<br>");
+document.write("A soma de " + num1 + " e " + num2 + " = " + soma + "<br>");
+document.write("A subtração de " + num1 + " e " + num2 + " = " + sub+ "<br>");
+document.write("A multiplicação de " + num1 + " e " + num2 + " = " + multi + "<br>");
 document.write("A divisão de " + num1 + " e " + num2 + " = " + div.toFixed(2) + "<br>");
-document.write("O resto da divisão de " + num1 + " e " + num2 + " = " + rest.toFixed(2) + "<br>");
-document.write("A potência de " + num1 + " elevado a " + num2 + " = " + pot.toFixed(2) + "<br>");
+document.write("O resto da divisão de " + num1 + " e " + num2 + " = " + rest + "<br>");
+document.write("A potência de " + num1 + " elevado a " + num2 + " = " + pot + "<br>");
 document.write("A raiz " + num1 + " de  " + num2 + " = " + raiz1.toFixed(2) + "<br>");
