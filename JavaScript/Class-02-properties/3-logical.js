f0956e295a91f32ee42ac7ab97a5549cda6410aa
Odilon Enrique <odilonegduarte@gmail.com>
2023-09-21 15:33:33 -0300

    update

--- JavaScript/Class-02-properties/3-logical.js
+++ JavaScript/Class-02-properties/3-logical.js
@@ -15,3 +15,4 @@ console.log(!saveMoney || getLoan);
 console.log(saveMoney || getLoan);
 console.log(saveMoney || !getLoan);
 console.log(saveMoney || !getLoan);
+console.log(!saveMoney || !getLoan);
