9d7791c4296d079be7b1ac51f51353f45281afa7
Odilon Enrique <odilonegduarte@gmail.com>
2023-09-29 19:38:10 -0300

    update files

--- JavaScript/Class-04-data-maipulation/challenge4/challegeArray.js
+++ JavaScript/Class-04-data-maipulation/challenge4/challegeArray.js
@@ -1,15 +1,15 @@
 function deleteElementArray(nameElement, array) {
-    const index = array.indexOf(nameElement);
-    console.log(index);
-    index != -1 ? array.splice(index, 1): "";
-    return  array;
+  const index = array.indexOf(nameElement);
+  console.log(index);
+  console.log(index != -1 ? array.splice(index, 1) : "");
+  return array;
 }
 
-const techs = ["HTML", 10, "CSS", 20, "JS", 30,];
+const techs = ["HTML", 10, "CSS", 20, "JS", 30];
 
 console.log(deleteElementArray("JS", techs));
 console.log(deleteElementArray(30, techs));
 console.log(deleteElementArray("HTML", techs));
 console.log(deleteElementArray("XHTML", techs));
 console.log(deleteElementArray("CSS", techs));
-console.log(deleteElementArray("JS", techs)); 
+console.log(deleteElementArray("JS", techs));
