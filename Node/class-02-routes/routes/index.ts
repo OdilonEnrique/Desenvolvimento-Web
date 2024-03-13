1e1602ccb688da9a82bc11bf77fdc12914f7a6cb
Odilon Enrique <odilonegduarte@gmail.com>
2024-03-13 20:46:22 -0300

    add route params examples

--- Node/class-02-routes/routes/index.ts
+++ Node/class-02-routes/routes/index.ts
@@ -6,11 +6,30 @@ export const router = Router();
 //   res.send({ name: "Odilon" });
 // });
 
-router.get("/", (_req, res) => {
+router.get("/pokemon", (_req, res) => {
   res.send([
     { id: 1, name: "Bulbassaur" },
     { id: 150, name: "Mew" },
     { id: 4, name: "Charizard" },
     { id: 20, name: "Pikachu" },
   ]);
+});
+
+// route params
+// router.get("/user/:id/:name", (req, res) => {
+//   const { id, name } = req.params;
+//   res.send({ id, name });
+// });
+
+// query params
+// router.get("/user", (req, res) => {
+//   const { id, name, age } = req.query;
+//   res.send({ id, name, age });
+// });
+
+// body params
+router.post("/user", (req, rest) => {
+  const { id, name, age } = req.body;
+  console.log({ id, name, age });
+  rest.send({ status: "User create!" });
 });
--- Node/class-02-routes/src/server.ts
+++ Node/class-02-routes/src/server.ts
@@ -9,4 +9,7 @@ app.listen(port, () => {
   console.log(`Server is running on PORT ${port}...`);
 });
 
+// middleware
+app.use(express.json());
+
 app.use(router);
