4c8e6c9a7fe11de9c31577573811268eb7e61c00
Odilon Enrique <odilonegduarte@gmail.com>
2024-03-15 20:19:17 -0300

    add controllers update and delete

--- Node/class-03-controllers/src/controllers/userControllers.ts
+++ Node/class-03-controllers/src/controllers/userControllers.ts
@@ -5,15 +5,34 @@ export const userControllers = {
   create(req: Request, res: Response) {
     const { id, name, age } = req.body;
     if (id && name && age) {
-      res.json({ status: `user ${id} created!` });
+      res.status(201).json({ status: `user ${id} created!` });
       return;
     }
 
-    res.json({ status: `user not created!` });
+    res.status(400).json({ status: `user not created!` });
   },
 
   read(req: Request, res: Response) {
     const { id } = req.params;
-    res.json({ user: id });
+    res.status(200).json({ user: id });
+  },
+
+  update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, age } = req.body;
+
+    if (id && name && age) {
+      console.log("updated", { id, name, age });
+      res.status(200).json({ status: `user ${id} updated!` });
+      return;
+    }
+
+    res.status(400).json({ status: `user not updated!` });
+  },
+
+  delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    res.status(200).json({ status: `user ${id} deleted!` });
   },
 };
--- Node/class-03-controllers/src/routes/index.ts
+++ Node/class-03-controllers/src/routes/index.ts
@@ -3,5 +3,7 @@ import { userControllers } from "../controllers/userControllers";
 
 export const router = Router();
 
-router.post("/user", userControllers.create)
+router.post("/user", userControllers.create);
 router.get("/user/:id", userControllers.read);
+router.put("/user/:id", userControllers.update);
+router.delete("/user/:id", userControllers.delete);
