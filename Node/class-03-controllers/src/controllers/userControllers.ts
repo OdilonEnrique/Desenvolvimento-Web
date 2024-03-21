b27f45e5dba2de59295866a1f55e26dc87c2b77d
Odilon Enrique <odilonegduarte@gmail.com>
2024-03-20 21:08:11 -0300

    add middlewares errors

--- Node/class-03-controllers/src/controllers/userControllers.ts
+++ Node/class-03-controllers/src/controllers/userControllers.ts
@@ -1,15 +1,19 @@
-import { Response } from "express";
-import { Request } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export const userControllers = {
-  create(req: Request, res: Response) {
-    const { id, name, age } = req.body;
-    if (id && name && age) {
-      res.status(201).json({ status: `user ${id} created!` });
-      return;
-    }
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id, name, age } = req.body;
+      if (id && name && age) {
+        // console.log(a);
 
-    res.status(400).json({ status: `user not created!` });
+        return res.status(201).json({ status: `user ${id} created!` });
+      }
+
+      throw res.status(400).json({ status: `user not created!` });
+    } catch (error) {
+      next(error);
+    }
   },
 
   read(req: Request, res: Response) {
@@ -17,17 +21,20 @@ export const userControllers = {
     res.status(200).json({ user: id });
   },
 
-  update(req: Request, res: Response) {
-    const { id } = req.params;
-    const { name, age } = req.body;
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const { name, age } = req.body;
 
-    if (id && name && age) {
-      console.log("updated", { id, name, age });
-      res.status(200).json({ status: `user ${id} updated!` });
-      return;
+      if (id && name && age) {
+        console.log("updated", { id, name, age });
+        return res.status(200).json({ status: `user ${id} updated!` });
+      }
+
+      throw res.status(400).json({ status: `user not updated!` });
+    } catch (error) {
+      next(error);
     }
-
-    res.status(400).json({ status: `user not updated!` });
   },
 
   delete(req: Request, res: Response) {
