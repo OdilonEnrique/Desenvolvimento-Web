3ed973eb80004beaab3fb1efcaca1df7f7a98723
Odilon Enrique <odilonegduarte@gmail.com>
2024-04-05 20:35:16 -0300

    add server errors and user create validation

--- Node/class-05-authentication/src/controllers/userControllers.ts
+++ Node/class-05-authentication/src/controllers/userControllers.ts
@@ -1,8 +1,45 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
 
 export const userControllers = {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
+      const userSchema = z
+        .object({
+          name: z
+            .string({
+              required_error: "Nome obrigatório",
+              invalid_type_error: "Para o nome use somente texto",
+            })
+            .min(3, { message: "Nome com mínimo de 3 caracteres" })
+            .max(255, { message: "Tamanho máximo atingido para o nome" }),
+
+          email: z
+            .string({
+              required_error: "Email obrigatório",
+              invalid_type_error: "Somente texto",
+            })
+            .email({ message: "email inválido" })
+            .max(255, { message: "Tamanho máximo atingido para o email" }),
+
+          password: z
+            .string({
+              required_error: "Senha obrigatório",
+              invalid_type_error: "Para a senha use o tipo textual!",
+            })
+            .min(7, { message: "Senha com o mínimo de 7 caracteres" })
+            .max(255, { message: "Tamanho máximo atingido para a senha" })
+            .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{7,}$/, {
+              message:
+                "Senha fraca: deve conter pelo menos pelo menos uma letra minúscula, uma letra maiúscula e um dígito.",
+            }),
+        })
+        .strict();
+
+      const { name, email, password } = userSchema.parse(req.body);
+
+      console.log(name, email, password);
+
       return res.status(201).json({ message: "User created!" });
     } catch (error) {
       return next(error);
--- Node/class-05-authentication/src/server.ts
+++ Node/class-05-authentication/src/server.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import "dotenv/config";
 import { routes } from "./routes";
+import { appErrors } from "./errors/appErrors";
+import { pageNotFound } from "./errors/pageNotFound";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(routes);
+
+app.use(pageNotFound);
+app.use(appErrors);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
